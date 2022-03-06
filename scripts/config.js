46342dfe8ede0a435532cbd89d135f3fdb6cde42
tangxianwen <1658172479@qq.com>
2022-03-06 21:16:55 +0800

    feat: 完善rollup配置

--- scripts/config.js
+++ scripts/config.js
@@ -1,26 +1,35 @@
-import replace from '@rollup/plugin-replace';
-const version = require('../package.json').version
+const path = require('path')
+const replace = require('@rollup/plugin-replace')
+const {babel} = require('@rollup/plugin-babel')
+const {nodeResolve} = require('@rollup/plugin-node-resolve')
+const {terser} = require("rollup-plugin-terser");
 
+const version = require('../package.json').version
+const resolve = p => {
+    return path.resolve(__dirname, '../', p)
+}
 const builds = {
-    umdDev: {
+    'umd-dev': {
         input: resolve('src/index.ts'),
         dest: resolve('dist/storage.js'),
         format: 'umd',
         env: 'development'
       },
-      umdProd: {
+      'umd-prod': {
         input: resolve('src/index.ts'),
         dest: resolve('dist/storage.min.js'),
         format: 'umd',
         env: 'production'
       },
-      esm: {
+      'esm': {
         input: resolve('src/index.ts'),
         dest: resolve('dist/storage.esm.js'),
         format: 'es'
       }
 
 }
+const extensions = ['.js', '.ts'];
+
 function genConfig (name) {
     const opts = builds[name]
     const config = {
@@ -29,14 +38,32 @@ function genConfig (name) {
         replace(   {
             __VERSION__: version
           }),
-        flow(),
-        alias(Object.assign({}, aliases, opts.alias))
-      ].concat(opts.plugins || []),
+          nodeResolve({
+              extensions,
+              modulesOnly: true,
+          }),
+          babel({
+              exclude: 'node_modules/**',
+              extensions,
+          }),
+
+      ],
       output: {
         file: opts.dest,
         format: opts.format,
         name: 'storage-api'
       }
     }
+    const vars = {}
+    if (opts.env) {
+        vars['process.env.NODE_ENV'] = JSON.stringify(opts.env)
+    }
+
+    config.plugins.push(replace(vars))
+
+    if (opts.env === 'production'){
+        config.plugins.push(terser())
+    }
     return config
 }
+exports.getAllBuilds = () => Object.keys(builds).map(genConfig)
