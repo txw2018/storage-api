8fd42af5b31e100a42cbf27c0295f23e60d0d003
txw <1658172479@qq.com>
2022-03-07 19:01:15 +0800

    feat: 增加api

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,55 @@
-const sum = function (a: number, b: number): number {
-    return a + b;
-};
-console.log(sum(1, 2));
+
+export const LocalStorage = {
+    set(key: string, value: any) {
+        if (typeof value === 'string') {
+            localStorage.setItem(key, JSON.stringify({
+                _format: 'string',
+                value
+            }))
+        }
+        if (typeof value === 'object') {
+            localStorage.setItem(key, JSON.stringify({
+                _format: 'object',
+                value
+            }))
+        }
+    },
+    get(key: string): any {
+        const val = localStorage.getItem(key)
+        if (val === null) {
+            return null
+        }
+
+        return JSON.parse(val).value
+    },
+    remove(key: string) {
+        localStorage.removeItem(key)
+    }
+}
+
+export const SessionStorage = {
+    set(key: string, value: any) {
+        if (typeof value === 'string') {
+            sessionStorage.setItem(key, JSON.stringify({
+                _format: 'string',
+                value
+            }))
+        }
+        if (typeof value === 'object') {
+            sessionStorage.setItem(key, JSON.stringify({
+                _format: 'object',
+                value
+            }))
+        }
+    },
+    get(key: string) {
+        const val: any = sessionStorage.getItem(key)
+        if (val === null) {
+            return null
+        }
+        return JSON.parse(val).value
+    },
+    removeItem(key: string) {
+        sessionStorage.removeItem(key)
+    }
+}
